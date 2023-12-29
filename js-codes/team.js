652286466dcd399f001ed48ca4f02b2444d5cf0d
Afinnity1 <omotolafinni089@gmail.com>
2023-12-29 23:39:23 +0100

    commit

---
+++ js-codes/team.js
@@ -0,0 +1,20 @@
+//  Js function for switching inbetween pages    
+document.getElementById("navbar-home").onclick = function(){
+    window.location.href = 'index.html'
+    }
+
+document.getElementById("navbar-about").onclick = function(){
+    window.location.href = 'about.html'
+}
+
+document.getElementById("navbar-plans").onclick = function(){
+    window.location.href = 'pricing.html'
+}
+
+document.getElementById("navbar-team").onclick = function(){
+    window.location.href = 'team.html'
+}
+
+document.getElementById("navbar-contact").onclick = function(){
+    window.location.href = 'contact.html'
+}
